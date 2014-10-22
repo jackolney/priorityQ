88c14eb18106ab8afd587762fd80123a28f54f5b
Jack Olney <jack.olney@me.com>
2014-10-22 16:22:45 +0100

    if nextEvent !CheckValid() then Cancel()

--- priorityQ/eventQ.cpp
+++ priorityQ/eventQ.cpp
@@ -52,6 +52,8 @@ void eventQ::RunEvents()
 		PopTop();
 		if(nextEvent->CheckValid()) // Checks to see if event is cancelled, if so doesn't even bother doing anything else should just PopTop() it.
 			nextEvent->Execute();
+		else
+			nextEvent->Cancel();
 		delete nextEvent;
 		D(cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tcurrentTime is = " << currentTime << endl);
 		D(cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\ttheQ size is = " << theQ->Size() << endl);
