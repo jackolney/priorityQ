90bb2274fcce4ad0ecad2487d93808bac524e630
Jack Olney <jack.olney@me.com>
2014-11-05 17:42:37 +0000

    incidenceEvents creation.

---
+++ priorityQ/incidenceEvents.cpp
@@ -0,0 +1,9 @@
+//
+//  incidenceEvents.cpp
+//  priorityQ
+//
+//  Created by Jack Olney on 05/11/2014.
+//  Copyright (c) 2014 Jack Olney. All rights reserved.
+//
+
+#include "incidenceEvents.h"
---
+++ priorityQ/incidenceEvents.h
@@ -0,0 +1,14 @@
+//
+//  incidenceEvents.h
+//  priorityQ
+//
+//  Created by Jack Olney on 05/11/2014.
+//  Copyright (c) 2014 Jack Olney. All rights reserved.
+//
+
+#ifndef __priorityQ__incidenceEvents__
+#define __priorityQ__incidenceEvents__
+
+#include <stdio.h>
+
+#endif /* defined(__priorityQ__incidenceEvents__) */
