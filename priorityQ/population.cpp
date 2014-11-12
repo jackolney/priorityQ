76a379c60ccb99a8434f44383942b1d495ee4f7b
Jack Olney <jack.olney@me.com>
2014-11-12 15:03:27 +0000

    AddVector() in population.h

--- priorityQ/population.cpp
+++ priorityQ/population.cpp
@@ -85,13 +85,19 @@ void population::AddPerson(person * me)
 
 void population::AddToVector(person * me)
 {
-	if(me->GetGender()) {
-			//Male
-		me->GetAge();
-	} else {
-			//Female
-		
-	}
+	// 0to4,5to9,to14,to19,to24,to29,to34,to39,to44,to49,to54,to59,to64,to69,to74,to79,80
+	unsigned int ageCatMax[17] = {4,9,14,19,24,29,34,39,44,49,54,59,64,69,74,79,200};
+	unsigned int i = 0;
+	while(me->GetAge() > ageCatMax[i] && i < 17)
+		i++;
+	
+	if(me->GetGender())
+		i += 17;
+
+	if(!me->GetSeroStatus())
+		Susceptible.at(i).push_back(me);
+	else
+		Infected.at(i).push_back(me);
 }
 
 /////////////////////
